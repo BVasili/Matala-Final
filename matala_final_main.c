fea8c3b07011011dcf8f55ab349b7753d2eefb9d
BVasili <bvasiliev@icloud.com>
2024-03-24 21:57:59 +0200

    adding more functions

--- matala_final_main.c
+++ matala_final_main.c
@@ -1,10 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
 
 // Constants
 #define nMems 300
 #define IDdigits 10
+#define num_LoanBooks 4
 
 // Structures Declaration
 typedef struct
@@ -32,20 +37,29 @@ typedef struct
 
 // Function Declaration
 void print_member(LibMember *);
-void search_id(LibMember *);
+int search_id(LibMember *, int, char[]);
 void printdate(Date *);
 void printloanedbook(LibMember *, int);
 int menu();
+void add_member(LibMember *, int);
+void formatName(char *str);
+int hasNumber(const char *str);
+int isOnlyLetters(const char *str);
+int isOnlyNumbers(const char *str);
 
 // Main function
 int main()
 {
-	printf("hello world");
+	menu();
+	system("pause");
 }
 
-// Function Definition
+// Print loaned book information
 void printloanedbook(LibMember *ptr, int index)
 {
+	if (&(ptr->LoanBooks[index]) == NULL)
+		return;
+
 	Date *ptr2return = &(ptr->LoanBooks[index].ReturnDate);
 
 	printf("Name of book: %s\n", ptr->LoanBooks[index].BookName);
@@ -54,11 +68,13 @@ void printloanedbook(LibMember *ptr, int index)
 	printdate(ptr2return);
 }
 
+// Print date function
 void printdate(Date *ptr)
 {
 	printf("%d.%d.%d\n", ptr->Day, ptr->Month, ptr->Year);
 }
 
+// Prints information about library member
 void print_member(LibMember *ptr)
 {
 	Date *ptr2birth = &(ptr->DateOfBirth);
@@ -67,18 +83,31 @@ void print_member(LibMember *ptr)
 	printf("ID: %s\n", ptr->Id);
 	printf("Date of Birth: ");
 	printdate(ptr2birth);
+
+	for (int i = 0; i < num_LoanBooks; i++)
+		printloanedbook(ptr, i);
 }
 
-void search_id(LibMember *ptr)
+// Searches for ID in Database.
+int search_id(LibMember member_array[], int activeUsers, char id[])
 {
+	// Compares given ID to the database and returns index if found.
+	for (int i = 0; i < activeUsers; i++)
+	{
+		if (strcmp(member_array[i].Id, id))
+			return i;
+	}
+	// Returns -1 if ID is not within database.
+	return -1;
 }
 
+// Prints menu, returns menu choice as a short int.
 int menu()
 {
 	short int menu_choice = 0;
-
 	do
 	{
+		menu_choice = 0;
 		printf("Database System Menu:\n");
 		printf("1. Add member\n");
 		printf("2. Loan books\n");
@@ -87,13 +116,103 @@ int menu()
 		printf("5. Delete member\n");
 		printf("6. Display all members\n");
 		printf("7. Quit\n");
-		printf("Please choose:\n");
-		scanf(" %d\n", &menu_choice);
+		printf("Please choose an action:\n");
+		scanf("%d", &menu_choice);
 
-		if ((menu_choice < 1) && (menu_choice > 7))
-			printf("Error, Wrong input. Please try again.\n");
+		/* Checks if user inputed valid number - prints error and
+		   asks user to input valid number */
+		if ((menu_choice < 1) || (menu_choice > 7))
+			printf("\nError, Wrong input. Please try again.\n\n\n");
 
-	} while ((menu_choice < 1) && (menu_choice > 7));
+		// Flushes buffer to eliminate endless loop.
+		fseek(stdin, 0, SEEK_END);
+
+	} while ((menu_choice < 1) || (menu_choice > 7));
 
 	return menu_choice;
+}
+
+void formatName(char *str)
+{
+	*str = toupper(*str); // First letter to-be uppercase
+	while (*str)
+	{
+		*str = tolower(*str);
+
+		if (*(str - 1) == ' ')
+			*str = toupper(*str);
+
+		str++;
+	}
+}
+
+int hasNumber(const char *str)
+{
+	while (*str)
+	{
+		if (isdigit(*str))
+		{
+			return true; // Return true if a digit is found
+		}
+		str++;
+	}
+	return false; // Return false if no digit is found
+}
+
+int isOnlyLetters(const char *str)
+{
+	while (*str)
+	{
+		if (!isalpha(*str))
+		{
+			return false; // Return false if a non-alphabetic character is found
+		}
+		str++;
+	}
+	return true; // Return true if only letters are found
+}
+
+int isOnlyNumbers(const char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return false; // Return 0 if a non-numeric character is found
+		}
+		str++;
+	}
+	return true; // Return 1 if only numbers are found
+}
+
+void add_member(LibMember *ptr, int activeUsers)
+{
+	char name[40] = {'a'};
+	char id[9];
+	Date date_of_birth;
+	do
+	{ // Gets name and checks if valid
+		printf("Enter your name: ");
+		fgets(name, sizeof(name), stdin);
+
+		if (name[strlen(name) - 1] == '\n')
+			name[strlen(name) - 1] = '\0';
+
+		if (!isOnlyLetters(name))
+			printf("Error a valid name\n");
+
+	} while (!isOnlyLetters(name));
+
+	formatName(name);
+
+	do
+	{ // Gets ID numbers and check if its valid
+
+		printf("Enter your id: ");
+		fgets(id, sizeof(id), stdin);
+
+		if (!isOnlyNumbers(id))
+			printf("enter only numbers");
+
+	} while (!isOnlyNumbers(id));
 }
